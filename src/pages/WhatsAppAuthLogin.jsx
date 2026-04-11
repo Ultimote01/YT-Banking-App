a64b1b79996ec1189261512e53522e63d45cdffc
Nathaniel <ultimote01@hotmail.com>
2026-04-11 07:17:48 -0700

    error fix api

--- src/pages/WhatsAppAuthLogin.jsx
+++ src/pages/WhatsAppAuthLogin.jsx
@@ -147,6 +147,8 @@ export default function  WhatsAppAuthLogin() {
                 console.log(err)
                 }
     
+         }else{
+            navigate("/", {replace: true})
          }
   
 
