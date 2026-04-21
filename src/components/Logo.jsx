b8c0fc41027707478569573e3bf7c08d34497309
Nathaniel <ultimote01@hotmail.com>
2026-04-21 14:46:45 -0700

    fixing bug

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,8 +2,8 @@ export default function Logo({styleSpan1,...props}) {
     return(
         <div className={"logo-container"} {...props} >
              <img src="/yabatech_logo.jpg" className="logo-container-img"/>
-            <span className=" font-semibold text-xl py-[0.3rem] dark:text-white
-            add-logo text-zinc-950 " style={styleSpan1?? {}}>YT Banking App</span>
+            <span className=" font-semibold   py-[0.3rem] dark:text-white
+            text-zinc-950 " style={styleSpan1?? {}}>YT Banking App</span>
             </div>
     )
 }
